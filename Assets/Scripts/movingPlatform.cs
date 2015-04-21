edb443d3a501843341c24e1afbb3caa09d71af82
Paradosso <Paradossobay@gmail.com>
2015-04-21 10:10:15 +0200

    Livello 1: Aggiunta Piattaforma Mobile Aggiunti relativi prefab (Platform_moving) e script (movingPlatform).

---
+++ Assets/Scripts/movingPlatform.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movingPlatform : MonoBehaviour 
+{
+	public Transform destinationSpot;
+	public Transform originSpot;
+	public float speed;
+	public bool Switch = false;
+
+	//Nell'editor clonare la piattaforma 2 volte e chiamare le due copie destinationSpot e originSpot.
+	//Posizionarle nella scena e disattivare la loro sprite.
+	//Rendere trigger i loro collider ma lasciarli così da avere un'idea di dove si muoverà la piattaforma.
+
+	void FixedUpdate () 
+	{
+		if(transform.position == destinationSpot.position)
+		{
+			Switch = true;
+		}
+		if(transform.position == originSpot.position)
+		{
+			Switch = false;
+		}
+
+		if(Switch)
+		{
+			transform.position = Vector2.MoveTowards(transform.position, originSpot.position, speed);
+		}
+		else
+		{
+			transform.position = Vector2.MoveTowards(transform.position, destinationSpot.position, speed);
+		}
+	
+	}
+}
