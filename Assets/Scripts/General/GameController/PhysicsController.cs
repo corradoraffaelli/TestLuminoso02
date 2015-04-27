4a8f131f00527ced73853ca20147f40a8c3e2f22
Corrado Raffaelli <raffaelli.corrado@gmail.com>
2015-04-27 11:25:40 +0200

    Aggiunto script per gestire gravità dall'inspector

---
+++ Assets/Scripts/General/GameController/PhysicsController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysicsController : MonoBehaviour {
+
+	public float gravityValue = -9.81f;
+	float gravity;
+
+	bool valueChanged = false;
+
+	// Use this for initialization
+	void Start () {
+		gravity = gravityValue;
+		Physics2D.gravity = new Vector2 (0.0f, gravity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//aggiorna il valore di gravità solo se modificato dall'inspector, per debug
+		if (gravity != gravityValue) {
+			gravity = gravityValue;
+			Physics2D.gravity = new Vector2 (0.0f, gravity);
+		}
+	}
+}
