3e4e6209bd9417ee46e1a91eafa8eb0fc53b4802
Dario Randazzo <dar.randazzo@gmail.com>
2015-05-04 23:44:44 +0200

    acquario personalizzabile

--- Assets/acquariumHandle.cs
+++ Assets/acquariumHandle.cs
@@ -5,7 +5,29 @@ public class acquariumHandle : MonoBehaviour {
 
 	AreaEffector2D aeHorizontal;
 	AreaEffector2D aeVertical;
-	float maxForce = 15.0f;
+	AreaEffector2D aeLeftBarrier;
+	AreaEffector2D aeRightBarrier;
+	AreaEffector2D aeBasilarSpring;
+
+	[Range(0.1f,20.0f)]
+	public float maxHorizontalForce = 8.0f;
+	//public float minHorizontalForce = 5.0f;
+	[Range(0.1f,5.0f)]
+	public float tBetweenChangeHorForce = 0.5f;
+	[Range(0.1f,20.0f)]
+	public float maxVerticalForce = 10.0f;
+	[Range(0.1f,5.0f)]
+	public float tBetweenChangeVertForce = 0.5f;
+
+	public bool lateralBarrier = true;
+	[Range(0.1f,20.0f)]
+	public float lateralBarrierForce = 10.0f;
+	public bool basilarSpring = true;
+	[Range(0.1f,20.0f)]
+	public float basilarSpringForce = 10.0f;
+	//public float minVerticalForce = 7.0f;
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -30,26 +52,66 @@ public class acquariumHandle : MonoBehaviour {
 				
 			}
 
+			if(child.name=="BasilarSpring") {
+				
+				aeBasilarSpring = child.GetComponent<AreaEffector2D> ();
+				
+			}
+
+			if(child.name=="BarrierLeft") {
+				
+				aeLeftBarrier = child.GetComponent<AreaEffector2D> ();
+				
+			}
+
+			if(child.name=="BarrierRight") {
+				
+				aeRightBarrier = child.GetComponent<AreaEffector2D> ();
+				
+			}
+
+
 		}
 
 	}
 
 	// Update is called once per frame
 	void Update () {
-	
+
+		if(lateralBarrier){
+			aeLeftBarrier.forceMagnitude = lateralBarrierForce;
+			aeRightBarrier.forceMagnitude = lateralBarrierForce;
+		}
+		else {
+
+			aeLeftBarrier.forceMagnitude = 0.0f;
+			aeRightBarrier.forceMagnitude = 0.0f;
+
+		}
+
+		if (basilarSpring) {
+			aeBasilarSpring.forceMagnitude = basilarSpringForce;
+		}
+		else {
+			aeBasilarSpring.forceMagnitude = 0.0f;
+
+		}
+
+
+
 	}
 
 	private IEnumerator changeVerticalWaves() {
 		
 		while (true) {
 
-			yield return new WaitForSeconds (2.0f);
+			yield return new WaitForSeconds ( tBetweenChangeVertForce / 2);
 
-			aeVertical.forceMagnitude = 10.0f;
+			aeVertical.forceMagnitude = maxVerticalForce / 2;
 
-			yield return new WaitForSeconds (1.0f);
+			yield return new WaitForSeconds ( tBetweenChangeVertForce / 2);
 			
-			aeVertical.forceMagnitude = 7.0f;
+			aeVertical.forceMagnitude = maxVerticalForce;
 
 		}
 
@@ -58,18 +120,18 @@ public class acquariumHandle : MonoBehaviour {
 	private IEnumerator changeHorizontalWaves() {
 
 		while (true) {
-			yield return new WaitForSeconds (2.0f);
+			yield return new WaitForSeconds ( tBetweenChangeHorForce );
 
 			if(aeHorizontal.forceMagnitude>0.0f) {
-				aeHorizontal.forceMagnitude = -3.0f;
-				yield return new WaitForSeconds (0.5f);
-				aeHorizontal.forceMagnitude = -6.0f;
+				aeHorizontal.forceMagnitude = - maxHorizontalForce / 2 ;
+				yield return new WaitForSeconds ( tBetweenChangeHorForce );
+				aeHorizontal.forceMagnitude = - maxHorizontalForce;
 			}
 			else {
 
-				aeHorizontal.forceMagnitude = 3.0f;
-				yield return new WaitForSeconds (0.5f);
-				aeHorizontal.forceMagnitude = 6.0f;
+				aeHorizontal.forceMagnitude = maxHorizontalForce;
+				yield return new WaitForSeconds (tBetweenChangeHorForce);
+				aeHorizontal.forceMagnitude = maxHorizontalForce;
 
 			}
 
