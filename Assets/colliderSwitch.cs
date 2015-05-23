4d4656e3f113ed20574173f546a255b58d7821d0
Paradosso <Paradossobay@gmail.com>
2015-05-23 17:28:04 +0200

    - Aggiunti Tag e Layer: SuperPlayer - Scena Test: 03_Super-Platform_Test - Creazione Super-Piattaforma Test - Aggiunto Player Test (cubo)

---
+++ Assets/colliderSwitch.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class colliderSwitch : MonoBehaviour 
+{
+	public bool super = false;
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			other.tag = "SuperPlayer";
+			Debug.Log("Cambiato Tag in:" + other.tag);
+			other.gameObject.layer = 19;
+			super = true;
+		}
+
+			
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.CompareTag("SuperPlayer"))
+		{
+			other.tag = "Player";
+			Debug.Log("Cambiato Tag in:" + other.tag);
+			other.gameObject.layer = 12;
+			super = false;
+		}
+			
+	}
+
+	/*void Change
+	{
+		if (super)
+
+
+	}*/
+}
---
+++ Assets/movementTest.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movementTest : MonoBehaviour 
+{
+	public bool jump = false;
+	public float moveForce = 365f;			// Amount of force added to move the player left and right.
+	public float maxSpeed = 5f;
+	public float jumpForce = 1000f;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetButtonDown("Jump")) //(Input.GetButtonDown("Jump") && grounded)
+			jump = true;
+	}
+
+	void FixedUpdate ()
+	{
+		float h = Input.GetAxis("Horizontal");
+
+		if(h * GetComponent<Rigidbody2D>().velocity.x < maxSpeed)
+			// ... add a force to the player.
+			GetComponent<Rigidbody2D>().AddForce(Vector2.right * h * moveForce);
+		
+		// If the player's horizontal velocity is greater than the maxSpeed...
+		if(Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > maxSpeed)
+			// ... set the player's velocity to the maxSpeed in the x axis.
+			GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Sign(GetComponent<Rigidbody2D>().velocity.x) * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+	
+		if(jump)
+		{
+
+			
+
+			
+			// Add a vertical force to the player.
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+			
+			// Make sure the player can't jump again until the jump conditions from Update are satisfied.
+			jump = false;
+		}
+	}
+}
