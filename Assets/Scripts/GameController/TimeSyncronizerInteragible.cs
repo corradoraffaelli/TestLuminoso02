b9f0e037e99679cc13484b92816f327c3711e8c8
Corrado Raffaelli <raffaelli.corrado@gmail.com>
2015-06-29 16:37:08 +0200

    Aggiunta struttura base dell'hub. Modificato il gameController con uno script che gestisce un valore float normalizzato che va da 0 ad 1 ogni tot secondi. Sfruttato lo script di cui sopra per far "pulsare" gli oggetti interagibili ed anche altro.

---
+++ Assets/Scripts/GameController/TimeSyncronizerInteragible.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeSyncronizerInteragible : MonoBehaviour {
+
+	[SerializeField]
+	[Range(5.0f, 200.0f)]
+	float timeBeforePulse = 10.0f;
+	[SerializeField]
+	[Range(0.05f, 30.0f)]
+	float pulsingSpeed = 2.0f;
+	[SerializeField]
+	[Range(1, 10)]
+	int pulsingTimes = 4;
+
+	float lastPulseTime = 0.0f;
+
+	bool needToPulse;
+	public bool NeedToPulse{
+		get{return needToPulse;}
+	}
+
+	float normalizedValue = 1.0f;
+	public float NormalizedValue{
+		get{return normalizedValue;}
+	}
+
+	int actualPulsingTime = 0;
+
+	bool reachedMaxValue = true;
+	bool firstMin = false;
+
+	void Start () {
+		lastPulseTime = Time.time;
+	}
+
+	void Update () {
+		//bool startPulse = isTimeToPulse();
+		if (isTimeToPulse())
+			pulseHandler();
+	}
+
+	bool isTimeToPulse()
+	{
+		if (needToPulse || (Time.time - lastPulseTime) > timeBeforePulse)
+		{
+			lastPulseTime = Time.time;
+			return true;
+		}
+		else
+			return false;
+	}
+
+	void pulseHandler()
+	{
+		if  (actualPulsingTime < pulsingTimes)
+		{
+			needToPulse = true;
+			float destValue = 0.0f;
+			if (!reachedMaxValue)
+			{
+				destValue = 1.0f;
+			}
+			normalizedValue = Mathf.MoveTowards(normalizedValue, destValue, Time.deltaTime * pulsingSpeed);
+
+			if (normalizedValue == 1.0f)
+			{
+				reachedMaxValue = true;
+				actualPulsingTime ++;
+			}else if (normalizedValue == 0.0f)
+			{
+				reachedMaxValue = false;
+			}
+
+		}
+		else
+		{
+			actualPulsingTime = 0;
+			needToPulse = false;
+		}
+			
+	}
+}
