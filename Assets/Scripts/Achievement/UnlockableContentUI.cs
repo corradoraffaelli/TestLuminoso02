28d2cf9f51e6a0736bd495cb14efaf0a3ff1e3f0
Corrado Raffaelli <raffaelli.corrado@gmail.com>
2015-07-02 18:36:31 +0200

    Pushata prima versione dello script che si occupa della grafica dei contenuti sbloccati

---
+++ Assets/Scripts/Achievement/UnlockableContentUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnlockableContentUI : MonoBehaviour {
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+
+	public void unlockFragment(string id)
+	{
+		Debug.Log ("ho sbloccato il frammento "+id);
+	}
+
+	public void unlockContent(string name)
+	{
+		Debug.Log ("ho sbloccato l'oggetto "+name);
+	}
+}
