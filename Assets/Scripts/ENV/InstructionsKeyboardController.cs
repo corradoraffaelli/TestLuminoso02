0401bbd6bcdb6bb3f7ea0514f2f119ba2b0166ce
Corrado Raffaelli <raffaelli.corrado@gmail.com>
2015-07-14 10:57:18 +0200

    Aggiornati alcuni cartelli del livello 1

---
+++ Assets/Scripts/ENV/InstructionsKeyboardController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstructionsKeyboardController : MonoBehaviour {
+
+	public bool sameSprite = false;
+	public Sprite keyboardSprite;
+	public Sprite controllerSprite;
+
+	bool useController = false;
+	bool wasUseController = false;
+
+	SpriteRenderer spriteRenderer;
+
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	void Update () {
+		if (!sameSprite)
+		{
+			useController = GeneralFinder.cursorHandler.useController;
+			if (wasUseController != useController)
+				setKeyboardSprite();
+			wasUseController = useController;
+		}
+	}
+
+	void setKeyboardSprite()
+	{
+		if (spriteRenderer != null)
+		{
+			if (useController && controllerSprite != null)
+			{
+				spriteRenderer.sprite = controllerSprite;
+			}
+			else if (!useController && keyboardSprite != null)
+			{
+				spriteRenderer.sprite = keyboardSprite;
+			}
+		}
+	}
+}
