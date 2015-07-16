011d7e30dd171b1cf9d4dd867389dc61df891e7f
Corrado Raffaelli <raffaelli.corrado@gmail.com>
2015-07-16 20:19:21 +0200

    Aggiunto il suono dei gabbiani e del mare

---
+++ Assets/Scripts/ENV/Sound/SeaActivation.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeaActivation : MonoBehaviour {
+
+	public AudioHandler audioHandler;
+
+	public string seaSound = "Mare";
+	public string seagullsSound = "Gabbiani";
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			activeSound();
+		}
+	}
+
+	void activeSound()
+	{
+		if (audioHandler != null)
+		{
+			audioHandler.playClipByName("Mare");
+			audioHandler.playClipByName("Gabbiani");
+		}
+	}
+}
