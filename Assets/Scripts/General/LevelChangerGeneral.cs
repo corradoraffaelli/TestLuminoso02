04d89d8df02dacfb5ee06ba0d060287b475ff2ed
Corrado Raffaelli <raffaelli.corrado@gmail.com>
2015-09-16 18:59:02 +0200

    Primi test per schermata di cambio scena

---
+++ Assets/Scripts/General/LevelChangerGeneral.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelChangerGeneral : MonoBehaviour {
+
+	int loadProgress = 0;
+
+	public GameObject blackScreen;
+	public GameObject text;
+
+	UnityEngine.UI.Text textUI;
+
+	int progress;
+
+	public GameObject[] canvasToDisable;
+
+	void Start () {
+		if (text != null && blackScreen != null) {
+			blackScreen.SetActive(false);
+			text.SetActive(false);
+
+			textUI = text.GetComponent<UnityEngine.UI.Text>();
+		}
+	
+	}
+
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.Z))
+			loadLevel ("00_Level_01#7");
+	}
+
+	IEnumerator DisplayLoadingScreen(string level)
+	{
+		if (blackScreen != null && text != null) {
+			blackScreen.SetActive (true);
+			text.SetActive (true);
+
+			for (int i = 0; i < canvasToDisable.Length; i++)
+			{
+				if (canvasToDisable[i] != null)
+				{
+					canvasToDisable[i].SetActive(false);
+				}
+			}
+
+			AsyncOperation async = Application.LoadLevelAsync(level);
+
+			while (!async.isDone)
+			{
+				progress = (int)(async.progress * 100);
+				Debug.Log (progress);
+
+				textUI.text = progress.ToString();
+
+				yield return null;
+
+			}
+
+		}
+	}
+
+	public void loadLevel(string levelToLoad)
+	{
+		StartCoroutine (DisplayLoadingScreen (levelToLoad));
+	}
+}
