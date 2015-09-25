eee47413ccb29a3295862cb73795fe6f7df761ce
Corrado Raffaelli <raffaelli.corrado@gmail.com>
2015-09-25 19:39:46 +0200

    Aggiunta la possibilità di gestire diversi stadi della lanterna nei commentTrigger

--- Assets/Scripts/Player/CommentTrigger.cs
+++ Assets/Scripts/Player/CommentTrigger.cs
@@ -17,7 +17,38 @@ public class CommentTrigger : MonoBehaviour {
 
 	bool playerColliding = false;
 	bool playerCollidingOLD = false;
-	
+
+	MagicLantern.lanternState lanternState;
+	MagicLantern.lanternState lanternStateOLD;
+
+	public bool active = true;
+
+	public enum ActionOnObject{None, Enable, Disable};
+
+	[System.Serializable]
+	class ConsequencesElement{
+		public GameObject gameObject;
+		public string methodToCall;
+		public ActionOnObject actionOnObject;
+	}
+
+	[System.Serializable]
+	class TextElement{
+		public MagicLantern.lanternState[] lanternStates;
+		public string text = "Testo di prova";
+		public string textController = "";
+		public ConsequencesElement[] exitingConsequences;
+		//[HideInInspector]
+		public bool active = false;
+		[HideInInspector]
+		public bool activeOLD = false;
+		[HideInInspector]
+		public GameObject balloonCreated;
+	}
+
+	[SerializeField]
+	TextElement[] textElements;
+
 	void Start()
 	{
 		if (NPC == null)
@@ -26,6 +57,83 @@ public class CommentTrigger : MonoBehaviour {
 
 	void Update () {
 	
+		if (active) {
+			if (textElements.Length != 0)
+			{
+				lanternState = GeneralFinder.magicLanternLogic.actualState;
+				
+				if (playerColliding) {
+					if ((lanternState != lanternStateOLD) || (playerColliding != playerCollidingOLD))
+					{
+						for (int i = 0; i < textElements.Length; i++)
+						{
+							if (textElements[i] != null)
+							{
+								bool actualFoundActive = false;
+								
+								if (textElements[i].lanternStates.Length == 0)
+								{
+									textElements[i].active = true;
+									actualFoundActive = true;
+								}
+								
+								for (int j = 0; j < textElements[i].lanternStates.Length; j++)
+								{
+									if ((textElements[i].lanternStates[j] == MagicLantern.lanternState.None)
+									    || (textElements[i].lanternStates[j] == lanternState))
+									{
+										textElements[i].active = true;
+										actualFoundActive = true;
+										break;
+									}
+								}
+								if (actualFoundActive)
+									continue;
+								setActiveFalse(i);
+							}
+							
+							
+						}
+						
+					}
+				}
+				
+				if (playerColliding != playerCollidingOLD) {
+					
+					if (playerColliding)
+					{
+						
+					}
+					else
+					{
+						setAllActiveFalse();
+					}
+				}
+				
+				activeBehaviour ();
+				
+				updateActiveOLD ();
+				lanternStateOLD = lanternState;
+				playerCollidingOLD = playerColliding;
+			}
+
+			//non ci sono elementi nel textElements
+			else
+			{
+				if (playerColliding != playerCollidingOLD) {
+					if (playerColliding)
+						showDialogue02();
+					else
+						Destroy(balloonCreated);
+				}
+				
+				playerCollidingOLD = playerColliding;
+			}
+		}
+
+
+
+		/*
 		if (playerColliding != playerCollidingOLD) {
 			if (playerColliding)
 				showDialogue();
@@ -34,10 +142,27 @@ public class CommentTrigger : MonoBehaviour {
 		}
 
 		playerCollidingOLD = playerColliding;
+		*/
 
 	}
 
-	void OnTriggerEnter2D(Collider2D other)
+	void setAllActiveFalse()
+	{
+		for (int i = 0; i < textElements.Length; i++)
+			setActiveFalse (i);
+	}
+
+	void setActiveFalse(int index)
+	{
+		if (index < textElements.Length){
+			//Debug.Log ("entrato");
+			if (textElements [index] != null) {
+				textElements [index].active = false;
+			}
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D other)
 	{
 		if (other.gameObject.tag == "Player")
 			playerColliding = true;
@@ -49,14 +174,14 @@ public class CommentTrigger : MonoBehaviour {
 			playerColliding = false;
 	}
 
-	void showDialogue()
+	void showDialogue(TextElement textElement, string inputText)
 	{
 		if (balloonPrefab != null) {
-			balloonCreated = Instantiate(balloonPrefab);
-			ComicBalloonManager balloonManager = balloonCreated.GetComponent<ComicBalloonManager>();
+			textElement.balloonCreated = Instantiate(balloonPrefab);
+			ComicBalloonManager balloonManager = textElement.balloonCreated.GetComponent<ComicBalloonManager>();
 			balloonManager.setType(ComicBalloonManager.Type.dialogue);
 
-			balloonManager.setText(text);
+			balloonManager.setText(inputText);
 			
 			balloonManager.setObjectToFollow(NPC);
 
@@ -67,6 +192,100 @@ public class CommentTrigger : MonoBehaviour {
 			balloonManager.setDialogueDownOffset(-dialogueDownOffset);
 
 			balloonManager.setDialogueOverOffset(dialogueOverOffset);
+		}
+	}
+
+	void showDialogue02()
+	{
+		if (balloonPrefab != null) {
+			balloonCreated = Instantiate(balloonPrefab);
+			ComicBalloonManager balloonManager = balloonCreated.GetComponent<ComicBalloonManager>();
+			balloonManager.setType(ComicBalloonManager.Type.dialogue);
+			
+			balloonManager.setText(text);
+			
+			balloonManager.setObjectToFollow(NPC);
+			
+			balloonManager.setDialogueContinue(false);
+			
+			balloonManager.setDialoguePosition(!balloonUnder);
+			
+			balloonManager.setDialogueDownOffset(-dialogueDownOffset);
+			
+			balloonManager.setDialogueOverOffset(dialogueOverOffset);
+		}
+	}
+
+	public void enable()
+	{
+		active = true;
+	}
+
+	public void disable()
+	{
+		for (int i = 0; i < textElements.Length; i++) {
+			if (textElements [i] != null) {
+				Destroy(textElements[i].balloonCreated);
+			}
+		}
+		Destroy(balloonCreated);
+		active = false;
+	}
+
+	void activeBehaviour()
+	{
+
+		for (int i = 0; i < textElements.Length; i++) {
+			if (textElements[i]!= null && (textElements[i].active != textElements[i].activeOLD))
+			{
+				if (textElements[i].active)
+				{
+					//Debug.Log ("Attivato "+i);
+					if (!GeneralFinder.cursorHandler.useController)
+						showDialogue(textElements[i], textElements[i].text);
+					else
+					{
+						if (textElements[i].textController != "")
+							showDialogue(textElements[i], textElements[i].textController);
+						else
+							showDialogue(textElements[i], textElements[i].text);
+					}
+				}
+					
+				else
+				{
+					//Debug.Log ("Disattivato "+i);
+					Destroy(textElements[i].balloonCreated);
+					callConsequences(textElements[i].exitingConsequences);
+				}
+					
+			}
+		}
+
+	}
+
+	void updateActiveOLD()
+	{
+		for (int i = 0; i < textElements.Length; i++) {
+			if (textElements[i] != null)
+				textElements[i].activeOLD = textElements[i].active;
+		}
+	}
+
+	void callConsequences(ConsequencesElement[] cons)
+	{
+		if (cons != null) {
+			for (int i = 0; i < cons.Length; i++)
+			{
+				if (cons[i] != null && cons[i].gameObject != null)
+				{
+					if (cons[i].actionOnObject == ActionOnObject.Enable)
+						cons[i].gameObject.SetActive(true);
+					if (cons[i].actionOnObject == ActionOnObject.Disable)
+						cons[i].gameObject.SetActive(false);
+					cons[i].gameObject.SendMessage(cons[i].methodToCall, SendMessageOptions.DontRequireReceiver);
+				}
+			}
 		}
 	}
 }
