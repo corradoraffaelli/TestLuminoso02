4236508d3002027611f96b58068f161ea7f9dc13
Corrado Raffaelli <raffaelli.corrado@gmail.com>
2015-09-30 10:14:52 +0200

    Aggiunte funzioni per salvare info sui questionari

---
+++ Assets/Scripts/Testing/NPCSurveyInfo.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCSurveyInfo : MonoBehaviour {
+
+	int contentIndex = 0;
+	int sectionIndex = 0;
+
+	public void c_setContentInt(int contentIndexInput)
+	{
+		contentIndex = contentIndexInput;
+	}
+
+	public void c_setSectionInt(int sectionIndexInput)
+	{
+		sectionIndex = sectionIndexInput;
+	}
+
+	public void wrongAnswer(int section, int content)
+	{
+		GeneralFinder.informativeManager.c_addWrongTrial(sectionIndex,contentIndex);
+	}
+
+	public void correctAnswer(int section, int content)
+	{
+		GeneralFinder.informativeManager.c_setQuestionAnswered(sectionIndex,contentIndex);
+	}
+}
